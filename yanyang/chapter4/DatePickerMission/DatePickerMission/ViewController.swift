83ed35e10dd4890eddbc9a54e24337eeaf1eb463
1003487 <1003487@SKP1003487MN001.local>
2017-06-20 18:52:30 +0900

    mission 3,4 commit

---
+++ yanyang/chapter4/DatePickerMission/DatePickerMission/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  DatePickerMission
+//
+//  Created by 1003487 on 2017. 6. 20..
+//  Copyright © 2017년 yanyangTest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    var alarmTime: String?
+    
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    @IBOutlet weak var lblPickerTime: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        alarmTime = formatter.string(from: datePickerView.date)
+    }
+    func updateTime(){
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+        
+        formatter.dateFormat = "hh:mm aaa"
+        let currentTime = formatter.string(from: date as Date)
+        
+        if (alarmTime == currentTime) {
+            view.backgroundColor = UIColor.red
+        }
+        else {
+            view.backgroundColor = UIColor.white
+        }
+    }
+
+}
+
