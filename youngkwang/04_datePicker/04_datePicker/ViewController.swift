8c4f623baa73276e780e4960019b605655db636b
조영광 <choyoungkwang@nhn.com>
2017-06-23 18:15:06 +0900

    영광 4장 datePicker 타이머? 추가

--- youngkwang/04_datePicker/04_datePicker/ViewController.swift
+++ youngkwang/04_datePicker/04_datePicker/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
     @IBOutlet weak var labelCurrent: UILabel!
     @IBOutlet weak var labelPickerTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +34,17 @@ class ViewController: UIViewController {
         
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
         labelPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    func updateTime(){
+        // labelCurrent.text = String(count)
+        // count = count + 1
+        
+        let date = NSDate()
+        let formatter = DateFormatter()
+        
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        labelCurrent.text = "현재시간: " + formatter.string(from: date as Date)
     }
 
 }
