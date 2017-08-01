97b340fc784e5e5505f148187d3cebd8b1f2f529
Jin.Lee <nt11212@nhn.com>
2017-08-01 19:00:57 +0900

    [Jin Lee] chapter10

---
+++ JinLee/chapter10/Tab/Tab/DateViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by NAVER on 2017. 6. 20..
+//  Copyright © 2017년 Jin Lee. All rights reserved.
+//
+
+import UIKit
+
+class DateViewController: UIViewController {
+    
+    let timeSelector: Selector = #selector(DateViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet weak var lblCurrentTime: UILabel!
+    
+    @IBOutlet weak var lblPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblPickerTime.text="선택시간 :" + formatter.string(from: datePickerView.date)
+        
+    }
+    func updateTime(){
+        lblCurrentTime.text = String(count)
+        count = count + 1
+        
+        let date = NSDate()
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        lblCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+}
+
---
+++ JinLee/chapter10/Tab/Tab/ImageViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  ImageBiew
+//
+//  Created by NAVER on 2017. 6. 20..
+//  Copyright © 2017년 Jin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var btnResize: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgOn = UIImage(named: "light.png")
+        imgOff = UIImage(named: "light_on.png")
+        
+        imgView.image = imgOn
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale:CGFloat = 2.0
+        var newWidth:CGFloat, newHeight:CGFloat
+        
+        if (isZoom) { //true
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            imgView.frame.size = CGSize(width: newWidth, height : newHeight)
+            btnResize.setTitle("확대", for: .normal)
+        
+        
+        }else{ //false
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            imgView.frame.size = CGSize(width: newWidth, height : newHeight)
+            btnResize.setTitle("축소", for: .normal)
+
+        }
+        
+        isZoom = !isZoom
+        
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn{
+            imgView.image = imgOn
+        }else{
+            imgView.image = imgOff
+        }
+    }
+    
+}
+
