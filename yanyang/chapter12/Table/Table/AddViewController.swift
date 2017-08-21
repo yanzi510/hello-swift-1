d4e9f43815b2c1deefb558556e462b77b1616f52
1003487 <1003487@SKP1003487MN001.local>
2017-08-21 19:01:31 +0900

    12장 완료

---
+++ yanyang/chapter12/Table/Table/AddViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddViewController.swift
+//  Table
+//
+//  Created by 1003487 on 2017. 8. 21..
+//  Copyright © 2017년 yanyangTest. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var tfAddItem: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnAddItem(_ sender: UIButton) {
+        items.append(tfAddItem.text!)
+        itemsImageFile.append("clock.png")
+        tfAddItem.text=""
+        _ = navigationController?.popViewController(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ yanyang/chapter12/Table/Table/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Table
+//
+//  Created by 1003487 on 2017. 8. 21..
+//  Copyright © 2017년 yanyangTest. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var receiveItem = ""
+
+    @IBOutlet weak var lblItem: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblItem.text = receiveItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func receiveItem(_ item: String) {
+        receiveItem = item
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
