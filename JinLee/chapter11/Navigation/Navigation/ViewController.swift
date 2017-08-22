6e963bbb8abf4207fa299275fc91df6e89f3545d
Jin.Lee <nt11212@nhn.com>
2017-08-22 19:01:03 +0900

    [Jin Lee] chapter11

---
+++ JinLee/chapter11/Navigation/Navigation/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by NAVER on 2017. 8. 22..
+//  Copyright © 2017년 Jin Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var txMessage: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let editViewController = segue.destination as! EditViewController
+        
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+        
+        
+    }
+}
+
