e2b5529cef0cc9a341af5faa596da3f84ef7a4ae
1003487 <1003487@SKP1003487MN001.local>
2017-09-04 15:56:43 +0900

    chapert14 commit

---
+++ yanyang/chapter14/MoviePlayer/MoviePlayer/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by 1003487 on 2017. 9. 4..
+//  Copyright © 2017년 yanyangTest. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "FastTyping", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+    }
+    
+    private func playVideo(url: NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+
+}
+
