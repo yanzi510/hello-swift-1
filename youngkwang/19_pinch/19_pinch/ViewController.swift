9f5367ca6ebbf088e3b75b872b0afeb9d620dea6
NAVER <NAVER@NAVERui-MacBook-Air-3.local>
2017-09-12 21:04:07 +0900

    영광 18장 pinch

---
+++ youngkwang/19_pinch/19_pinch/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  19_pinch
+//
+//  Created by NAVER on 2017. 9. 12..
+//  Copyright © 2017년 choyoungkwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imgPinch: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func doPinch(_ pinch: UIPinchGestureRecognizer){
+        imgPinch.transform = imgPinch.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        pinch.scale = 1
+    }
+
+}
+
